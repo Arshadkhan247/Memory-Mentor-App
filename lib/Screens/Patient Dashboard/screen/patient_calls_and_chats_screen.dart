184e7f203970ca5ab79dd8fbd762b09483660fac
Arshadkhan247 <ak4322866@gmail.com>
2024-03-08 02:46:21 +0500

    change in textformfield and add image icon.

--- lib/Screens/Patient Dashboard/screen/patient_calls_and_chats_screen.dart
+++ lib/Screens/Patient Dashboard/screen/patient_calls_and_chats_screen.dart
@@ -1,215 +1,11 @@
-// import 'package:flutter/material.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:intl/intl.dart';
+import 'dart:io';
 
-// class PatientCallsAndChatsScreen extends StatefulWidget {
-//   final String otherUserUid; // UID of the other user (patient or caregiver)
-
-//   const PatientCallsAndChatsScreen({Key? key, required this.otherUserUid})
-//       : super(key: key);
-
-//   @override
-//   _PatientCallsAndChatsScreenState createState() =>
-//       _PatientCallsAndChatsScreenState();
-// }
-
-// class _PatientCallsAndChatsScreenState
-//     extends State<PatientCallsAndChatsScreen> {
-//   final FirebaseAuth _auth = FirebaseAuth.instance;
-//   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-//   final TextEditingController _messageController = TextEditingController();
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Chat'),
-//       ),
-//       body: Stack(
-//         children: [
-//           // Background Image
-//           Image.asset(
-//             'assets/chatBack.jpg',
-//             fit: BoxFit.cover,
-//             width: double.infinity,
-//             height: double.infinity,
-//           ),
-//           Padding(
-//             padding: const EdgeInsets.all(8.0),
-//             child: Column(
-//               children: [
-//                 Expanded(
-//                   child: StreamBuilder<QuerySnapshot>(
-//                     stream: _firestore
-//                         .collection('messages')
-//                         .where('users', arrayContainsAny: [
-//                           _auth.currentUser?.uid,
-//                           widget.otherUserUid
-//                         ])
-//                         .orderBy('timestamp', descending: true)
-//                         .snapshots(),
-//                     builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-//                       if (snapshot.connectionState == ConnectionState.waiting) {
-//                         return const Center(child: CircularProgressIndicator());
-//                       }
-
-//                       if (snapshot.hasError) {
-//                         return Center(
-//                           child: Text('Error: ${snapshot.error}'),
-//                         );
-//                       }
-
-//                       if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-//                         return const Center(
-//                           child: Text("No messages"),
-//                         );
-//                       }
-
-//                       final messageDocs = snapshot.data!.docs;
-
-//                       return ListView.builder(
-//                         reverse: true,
-//                         itemCount: messageDocs.length,
-//                         itemBuilder: (context, index) {
-//                           final message =
-//                               messageDocs[index].data() as Map<String, dynamic>;
-//                           final messageText = message['text'] as String?;
-//                           final messageSender = message['sender'] as String?;
-//                           final timestamp = message['timestamp'] as Timestamp?;
-
-//                           return MessageWidget(
-//                             sender: messageSender ?? '',
-//                             text: messageText ?? '',
-//                             isMe: _auth.currentUser?.uid == messageSender,
-//                             timestamp: timestamp?.toDate() ?? DateTime.now(),
-//                           );
-//                         },
-//                       );
-//                     },
-//                   ),
-//                 ),
-//                 _buildMessageComposer(),
-//               ],
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-
-//   Widget _buildMessageComposer() {
-//     return Padding(
-//       padding: const EdgeInsets.all(8.0),
-//       child: Row(
-//         children: [
-//           Expanded(
-//             child: TextField(
-//               controller: _messageController,
-//               decoration: const InputDecoration(
-//                 hintText: 'Enter your message...',
-//               ),
-//             ),
-//           ),
-//           IconButton(
-//             icon: const Icon(Icons.send),
-//             onPressed: () async {
-//               final user = _auth.currentUser;
-//               if (user != null && _messageController.text.isNotEmpty) {
-//                 await _firestore.collection('messages').add({
-//                   'text': _messageController.text,
-//                   'sender': user.uid,
-//                   'receiver': widget.otherUserUid,
-//                   'timestamp': FieldValue.serverTimestamp(),
-//                   'users': [user.uid, widget.otherUserUid],
-//                 });
-//                 _messageController.clear();
-//               }
-//             },
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-// class MessageWidget extends StatelessWidget {
-//   final String sender;
-//   final String text;
-//   final bool isMe;
-//   final DateTime timestamp;
-
-//   const MessageWidget({
-//     Key? key,
-//     required this.sender,
-//     required this.text,
-//     required this.isMe,
-//     required this.timestamp,
-//   }) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Padding(
-//       padding: const EdgeInsets.all(8.0),
-//       child: Column(
-//         crossAxisAlignment:
-//             isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-//         children: [
-//           IntrinsicWidth(
-//             child: Container(
-//               constraints: BoxConstraints(
-//                   maxWidth: MediaQuery.of(context).size.width * 0.85),
-//               decoration: BoxDecoration(
-//                 color: isMe ? Colors.green[100] : Colors.grey.shade400,
-//                 borderRadius: BorderRadius.circular(8.0),
-//               ),
-//               padding: const EdgeInsets.all(4.0),
-//               child: Column(
-//                 crossAxisAlignment: CrossAxisAlignment.start,
-//                 children: [
-//                   Text(
-//                     text,
-//                     style: const TextStyle(fontSize: 16.0),
-//                   ),
-//                   const SizedBox(height: 4.0),
-//                   Align(
-//                     alignment: Alignment.bottomRight,
-//                     child: Text(
-//                       _formatTimestamp(timestamp),
-//                       style: const TextStyle(
-//                         fontSize: 11.0,
-//                         color: Colors.black54,
-//                       ),
-//                     ),
-//                   ),
-//                 ],
-//               ),
-//             ),
-//           )
-//         ],
-//       ),
-//     );
-//   }
-
-//   String _formatTimestamp(DateTime timestamp) {
-//     final now = DateTime.now();
-//     final today = DateTime(now.year, now.month, now.day);
-//     final yesterday = DateTime(now.year, now.month, now.day - 1);
-
-//     if (timestamp.isAfter(today)) {
-//       return 'Today ${DateFormat.jm().format(timestamp.toLocal())}';
-//     } else if (timestamp.isAfter(yesterday)) {
-//       return 'Yesterday ${DateFormat.jm().format(timestamp.toLocal())}';
-//     } else {
-//       return DateFormat('MMMM d, yyyy ${DateFormat.jm().pattern}')
-//           .format(timestamp.toLocal());
-//     }
-//   }
-// }
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:intl/intl.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class PatientCallsAndChatsScreen extends StatefulWidget {
   final String otherUserUid;
@@ -227,12 +23,14 @@ class _PatientCallsAndChatsScreenState
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final TextEditingController _messageController = TextEditingController();
+  final ImagePicker _imagePicker = ImagePicker();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.teal,
+        automaticallyImplyLeading: true,
+        backgroundColor: Colors.green.shade200,
         title: const Text('Chat'),
       ),
       body: Stack(
@@ -286,12 +84,14 @@ class _PatientCallsAndChatsScreenState
                           final messageText = message['text'] as String?;
                           final messageSender = message['sender'] as String?;
                           final timestamp = message['timestamp'] as Timestamp?;
+                          final imageUrl = message['image_url'] as String?;
 
                           return MessageWidget(
                             sender: messageSender ?? '',
                             text: messageText ?? '',
                             isMe: _auth.currentUser?.uid == messageSender,
                             timestamp: timestamp?.toDate() ?? DateTime.now(),
+                            imageUrl: imageUrl,
                             onDelete: () {
                               // Handle message deletion
                               _deleteMessage(messageDocs[index].id);
@@ -312,44 +112,90 @@ class _PatientCallsAndChatsScreenState
   }
 
   Widget _buildMessageComposer() {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Row(
-        children: [
-          Expanded(
-            child: TextField(
-              controller: _messageController,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(
-                      14,
-                    ),
+    return Row(
+      children: [
+        IconButton(
+          onPressed: _pickImage,
+          icon: const Icon(
+            Icons.image,
+          ),
+        ),
+        Expanded(
+          child: TextField(
+            controller: _messageController,
+            decoration: const InputDecoration(
+              contentPadding: EdgeInsets.only(left: 10),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(
+                    40,
                   ),
                 ),
-                hintText: 'Enter your message...',
               ),
+              hintText: 'Enter your message...',
             ),
           ),
-          IconButton(
-            icon: const Icon(Icons.send),
-            onPressed: () async {
-              final user = _auth.currentUser;
-              if (user != null && _messageController.text.isNotEmpty) {
-                await _firestore.collection('messages').add({
-                  'text': _messageController.text,
-                  'sender': user.uid,
-                  'receiver': widget.otherUserUid,
-                  'timestamp': FieldValue.serverTimestamp(),
-                  'users': [user.uid, widget.otherUserUid],
+        ),
+        IconButton(
+          icon: const Icon(Icons.send),
+          onPressed: () async {
+            final user = _auth.currentUser;
+            if (user != null) {
+              if (_messageController.text.isNotEmpty) {
+                await _sendMessage(_messageController.text, user.uid);
+              } else if (_imageFile != null) {
+                await _sendImage(_imageFile!, user.uid);
+                setState(() {
+                  _imageFile = null; // Clear the selected image after sending
                 });
-                _messageController.clear();
               }
-            },
-          ),
-        ],
-      ),
+              _messageController.clear();
+            }
+          },
+        ),
+      ],
     );
+  }
+
+  Future<void> _pickImage() async {
+    final pickedFile =
+        await _imagePicker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        _imageFile = File(pickedFile.path);
+      });
+    }
+  }
+
+  File? _imageFile;
+
+  Future<void> _sendImage(File imageFile, String senderUid) async {
+    // Upload image to Firebase Storage
+    final storageRef = FirebaseStorage.instance
+        .ref()
+        .child('chat_images/${DateTime.now()}.png');
+    final UploadTask uploadTask = storageRef.putFile(imageFile);
+    await uploadTask.whenComplete(() => null);
+    final imageUrl = await storageRef.getDownloadURL();
+
+    // Send message with image URL
+    await _firestore.collection('messages').add({
+      'image_url': imageUrl,
+      'sender': senderUid,
+      'receiver': widget.otherUserUid,
+      'timestamp': FieldValue.serverTimestamp(),
+      'users': [senderUid, widget.otherUserUid],
+    });
+  }
+
+  Future<void> _sendMessage(String text, String senderUid) async {
+    await _firestore.collection('messages').add({
+      'text': text,
+      'sender': senderUid,
+      'receiver': widget.otherUserUid,
+      'timestamp': FieldValue.serverTimestamp(),
+      'users': [senderUid, widget.otherUserUid],
+    });
   }
 
   void _deleteMessage(String messageId) async {
@@ -363,6 +209,7 @@ class MessageWidget extends StatelessWidget {
   final bool isMe;
   final DateTime timestamp;
   final VoidCallback onDelete;
+  final String? imageUrl;
 
   const MessageWidget({
     Key? key,
@@ -371,6 +218,7 @@ class MessageWidget extends StatelessWidget {
     required this.isMe,
     required this.timestamp,
     required this.onDelete,
+    this.imageUrl,
   }) : super(key: key);
 
   @override
@@ -386,6 +234,12 @@ class MessageWidget extends StatelessWidget {
           crossAxisAlignment:
               isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
           children: [
+            if (imageUrl != null)
+              Image.network(
+                imageUrl!,
+                width: 200.0, // Adjust the width as needed
+                height: 150.0, // Adjust the height as needed
+              ),
             IntrinsicWidth(
               child: Container(
                 constraints: BoxConstraints(
@@ -398,10 +252,11 @@ class MessageWidget extends StatelessWidget {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      text,
-                      style: const TextStyle(fontSize: 16.0),
-                    ),
+                    if (imageUrl == null)
+                      Text(
+                        text,
+                        style: const TextStyle(fontSize: 16.0),
+                      ),
                     const SizedBox(height: 4.0),
                     Align(
                       alignment: Alignment.bottomRight,
